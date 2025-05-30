7fa0b3e78e64670082c4d84980b4705959c8eab4
Kasper Overgård Nielsen <kasper@byolimit.com>
2025-05-30 17:32:40 +0200

    docs: Spin deeply_immutable_node into an example

--- lib/src/deeply_immutable/deeply_immutable_node.dart
+++ lib/src/deeply_immutable/deeply_immutable_node.dart
@@ -1,23 +1,30 @@
 // Copyright 2024 - 2025, kasper@byolimit.com
 // SPDX-License-Identifier: BSD-3-Clause
+import 'dart:isolate';
+
 import 'package:meta/meta.dart';
+import 'package:treap/treap.dart';
 
-import '../node.dart';
-
-// NOTE: This is in reality useless to outside users as it has to be sealed when
-// marked as deeply immutable, but it's here to show a user can easily create
-// treaps supporting deeply immutable data structures for efficient inter-isolate
-// communication.
+// NOTE: Classes has to be sealed or final when marked as deeply immutable.
+//
+// This is the reason the package cannot provide a common base class for
+// deeply immutable nodes.
 //
 // Hopefully, the Dart team will add support for an interface class for deeply
-// immutable classes in the future, so we can remove this class, and simplify
-// IntNode, and friends.
+// immutable classes in the future, so this can become part of the package
+//
+// Until then this example shows how to roll your own.
+
+// All your deeply immutable classes should inherit from this base class.
+// Copy this to your own project.
 @immutable
 @pragma('vm:deeply-immutable')
 sealed class DeeplyImmutable {
   const DeeplyImmutable();
 }
 
+// This is the node type to use for all deeply immutable classes.
+// Copy this to your own project.
 @immutable
 @pragma('vm:deeply-immutable')
 final class DeeplyImmutableNode<T extends DeeplyImmutable>
@@ -55,4 +62,46 @@ final class DeeplyImmutableNode<T extends DeeplyImmutable>
   DeeplyImmutableNode<T> withChildren(
           DeeplyImmutableNode<T>? left, DeeplyImmutableNode<T>? right) =>
       DeeplyImmutableNode(item, priority, left, right);
+}
+
+// Has to go in same file as DeeplyImmutable as
+// the base has to be sealed.
+final class Person extends DeeplyImmutable {
+  // Only deeply immutable members allowed
+  final String name;
+  final int age;
+
+  // You probably want to override == and hashCode, in order to use Person in
+  // sets or as keys in maps. But you can always pass a custom compare function
+  // otherwise.
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    if (other is! Person) return false;
+    return name == other.name;
+  }
+
+  @override
+  int get hashCode => name.hashCode;
+
+  const Person(this.name, this.age);
+}
+
+// Define a factory function for the node type. This can be generic, so you
+// only need to define one, regardless of how many _item_ classes you need.
+DeeplyImmutableNode<T> deeplyImmutableNodeFactory<T extends DeeplyImmutable>(
+  T item,
+) =>
+    DeeplyImmutableNode(item, defaultPriority(item), null, null);
+
+// It is convenient with a typedef, as the signature is a bit repetetive.
+// If you want to avoid parsing the factory to the ctor, take a look at the
+// implementation of TreapSetOfInt, which is an extension type instead of a
+// typedef, to see how to achieve this.
+typedef TreapSetOfMyItem = TreapSetBase<Person, DeeplyImmutableNode<Person>>;
+
+Future<void> main() async {
+  final persons = TreapSetOfMyItem.of([], deeplyImmutableNodeFactory);
+
+  await Isolate.run(() {});
 }
